656af7d5ec57c13e422df6ebaaf2e23d4b273b45
ziikey <92870256+ceekaye-lipnica@users.noreply.github.com>
2023-08-08 23:16:40 +0800

    December 2020

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main {
+    cout << "The original array elements are: " << endl;
+    cout << "LA[0] = 1 " << endl;
+    cout << "LA[1] = 3 " << endl;
+    cout << "LA[2] = 5 " << endl;
+    cout << "LA[3] = 7 " << endl;
+    cout << "LA[4] = 8 " << endl;
+
+    cout << endl;
+
+    cout << "The array elements after updation:" << endl;
+    cout << "LA[0] = 1 " << endl;
+    cout << "LA[1] = 3 " << endl;
+    cout << "LA[2] = 10 " << endl;
+    cout << "LA[3] = 7 " << endl;
+    cout << "LA[4] = 8 " << endl;
+
+return 0;
+}
